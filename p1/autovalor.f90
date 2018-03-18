4d94c42ef4e6761b331fcf3e1b42c517e625682e
solano <solano.felicio@protonmail.com>
2018-03-18 17:27:08 -0300

    Problema do autovalor, diagrama corrigido e programa melhorado

--- p1/autovalor.f90
+++ p1/autovalor.f90
@@ -3,10 +3,10 @@
 
         real*8, dimension(:,:), allocatable :: A
         real*8, dimension(:), allocatable :: x,y ! xk, xk+1
-        real*8 sigma, eps, lambda, lambda2, soma, norma, prod, normay, prec_x
+        real*8 sigma, eps, lambda, lambda2, prec_x, prod
         integer i,j,k,n,kmax
 
-        parameter (eps = 1e-8) ! tolerância do erro do autovalor
+        parameter (eps = 1.d-8) ! tolerância do erro do autovalor
         parameter (kmax = 50)  ! número máximo de iterações
 
         read(*,*) n
@@ -18,7 +18,7 @@
         do i=1,n
             read(*,*) ( A(i,j), j=1,n )
         end do
-        
+
         ! Chutar vetor x0 = (1,1,...,1)/sqrt(n)
         do i=1,n
             x(i) = 1/sqrt(float(n))
@@ -32,40 +32,24 @@
 
         !!!!!! LOOP !!!!!!
         sigma = 10 ! qualquer coisa maior que eps para começar
-        do while(k <= kmax .and. sigma >= eps)          
-            ! Normalizar xk+1
-            normay = norma(y,n)
-            do i=1,n
-                y(i) = y(i)/normay
-            end do
-            prec_x = norma(x-y,n)
+        do while(k <= kmax .and. sigma >= eps)
+            ! Norma induzida pelo produto interno
+            y = y/sqrt(prod(y,y,n))
+            prec_x = sqrt(prod(x-y,x-y,n))
 
             k = k+1
             x = y
-            
+
             call multmv(y,A,x,n)
             lambda2 = prod(x,y,n)
             sigma = abs(lambda2 - lambda)
             lambda = lambda2
         end do
-        
-        write(*,*) 'autovalor,sigma,prec_x=', lambda,sigma,prec_x
+
+        write(*,*) lambda, sigma, prec_x
 
         end program autovalor
 
-        ! Norma l2
-        function norma(x,n)
-            real*8 x(n)
-            integer i
-            real*8 norma
-            norma = 0
-            do i=1,n
-                norma = norma + x(i)**2
-            end do
-            norma = sqrt(norma)
-            return
-        end function
-        
         ! Produto interno
         function prod(x,y,n)
             real*8 prod
